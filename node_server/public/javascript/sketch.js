eaac7cb1acabaf3aa5035a498c42615aedc07569
rylanperumal <rylan.perumal@gmail.com>
2019-09-25 18:53:58 +0200

    Working project before Terence suggestions

--- node_server/public/javascript/sketch.js
+++ node_server/public/javascript/sketch.js
@@ -20,6 +20,17 @@ function setup() {
     const tiles = L.tileLayer(tileURL, {
         attribution
     });
+    // var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    //     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    // });
+    // var Stamen_Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+    //     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    //     subdomains: 'abcd',
+    //     minZoom: 0,
+    //     maxZoom: 18,
+    //     ext: 'png'
+    // });
+
     tiles.addTo(mymap);
     mymap.on('click', addMarker);
 
